3f5ae5f062ea181c95622f2dbb7c803eb7b3fdb2
Junyuan <conan007ai@gmail.com>
2012-04-23 21:08:33 +0800

    replace pagedown with markdown

--- routes/article.js
+++ routes/article.js
@@ -3,14 +3,12 @@
  * Article Related Routes.
  */
 
-var Converter = require('../node_modules/pagedown/Markdown.Converter').Converter
-  , conv = new Converter();
+markdown = require('markdown').markdown;
 
 exports.addarticle = function(req, res){
   res.render('article_add_page', { title: 'Weclome to Doc' });
 };
 
 exports.submitarticle = function(req, res){
-	console.log(conv.makeHtml(req.body.content));
-	res.render('aritcle_submit_status_page',{ title: 'result', content: conv.makeHtml(req.body.content)});
+	res.render('aritcle_submit_status_page',{ title: 'result', content: markdown.toHTML(req.body.content)});
 }
