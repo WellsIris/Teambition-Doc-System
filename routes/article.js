74861859014a50774fe5fbfcc6ba8dbfa7bae39e
Junyuan Qi <conan007ai@gmail.com>
2012-04-26 13:24:31 +0800

    full-featured editor based on makeitup plugin and some style fix
    also temporarily removed the preview page after submit and simply
    redirect to the index page;
    added style to highlight current page;
    better looking add button;

--- routes/article.js
+++ routes/article.js
@@ -9,7 +9,7 @@ var Converter = require("pagedown/Markdown.Converter.js").Converter;
 var converter = new Converter();
 
 exports.addarticle = function(req, res){
-  res.render('article_add_page', { title: '撰写新文档' });
+  res.render('article_add_page', {layout:false, title: '撰写新文档' });
 };
 
 exports.submitarticle = function(req, res){
@@ -18,5 +18,5 @@ exports.submitarticle = function(req, res){
 	article.category = req.body.category;
 	article.content = converter.makeHtml(req.body.content);
 	article.save();
-	res.render('aritcle_submit_status_page',{ layout: false, title: 'result', content: converter.makeHtml(req.body.content)});
+	res.redirect('/');
 }
--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,6 @@ exports.index = function(req, res){
 		for(var i = 0; i < docs.length; i++){
 			articles.push(docs[i]);
 		}
-		console.log(articles);
 		res.render('index', { title: 'Teambition 官方文档中心', description: '本文档提供了Teambition API的接口信息及使用范例', articles: articles });
 	});	
 };
